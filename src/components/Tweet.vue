e737433eb836cb75a0bcc84d8c79286a1d25318d
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-18 05:25:50 +0800

    feat: new post

---
+++ src/components/Tweet.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { isClient } from '@vueuse/core'
+import { onMounted, watch } from 'vue'
+import { isDark } from '../logics'
+
+if (isClient) {
+  onMounted(() => {
+  // @ts-ignore
+    window?.twttr.widgets.load()
+  })
+}
+</script>
+
+<template>
+  <div class="">
+    <blockquote class="twitter-tweet" :data-theme="isDark ? 'dark': 'light'">
+      <slot />
+    </blockquote>
+  </div>
+</template>
