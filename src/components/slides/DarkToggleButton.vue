d667f549313c095553d3d60c1a70136b95a58ef5
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-29 17:47:07 +0800

    feat: compsable vue

---
+++ src/components/slides/DarkToggleButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useToggle } from '@vueuse/core'
+import { isDark } from '/~/logics'
+
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <button
+    class="bg-[#42b883] rounded border-b-2 border-green-900 text-white text-sm px-2 pt-1.5 pb-1 inline-block !outline-none hover:bg-opacity-85"
+    @click="toggleDark"
+  >
+    <div class="flex">
+      <carbon:moon v-if="isDark" />
+      <carbon:sun v-else />
+      <span class="mr-1 ml-2">{{ isDark ? 'Dark' : 'Light' }}</span>
+    </div>
+  </button>
+</template>
---
+++ src/components/slides/Marker.vue
@@ -0,0 +1,3 @@
+<template>
+  <span class="bg-$prism-background px-2 py-1 rounded font-mono inline-block text-0.6em transform translate-y-[-1em] translate-x-[-0.3em]"><slot/></span>
+</template>
