5e2b18e4ef815b6b88ad055e743a1330152d67f6
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-30 20:46:33 +0800

    feat: new post

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,6 @@ const config: UserConfig = {
     ],
   },
   plugins: [
-    // @ts-expect-error
     Unocss({
       theme: {
         fontFamily: {
@@ -57,7 +56,14 @@ const config: UserConfig = {
         },
       },
       presets: [
-        presetIcons({ warn: true }),
+        presetIcons({
+          extraProperties: {
+            display: 'inline-block',
+            height: '1.2em',
+            width: '1.2em',
+            'vertical-align': 'text-bottom'
+          }
+        }),
         presetAttributify(),
         presetUno(),
       ],
