6ad290987bcae785816262b51997fc54ccae15ab
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-10 01:40:00 +0800

    chore: update

--- src/components/ListProjects.vue
+++ src/components/ListProjects.vue
@@ -11,18 +11,18 @@ defineProps<{ projects: Record<string, any[]> }>()
       <a
         v-for="item, idx in projects[key]"
         :key="idx"
-        class="item relative flex"
+        class="item relative flex items-center"
         :href="item.link"
         target="_blank"
         :class="!item.link ? 'opacity-0 pointer-events-none h-0 -mt-8 -mb-4' : ''"
       >
         <div v-if="item.icon" class="pt-2 pr-5">
-          <Slidev v-if="item.icon === 'slidev'" class="text-3xl opacity-50" />
-          <VueUse v-else-if="item.icon === 'vueuse'" class="text-3xl opacity-50" />
-          <VueReactivity v-else-if="item.icon === 'vue-reactivity'" class="text-3xl opacity-50" />
-          <VueDemi v-else-if="item.icon === 'vue-demi'" class="text-3xl opacity-50" />
-          <Unocss v-else-if="item.icon === 'unocss'" class="text-3xl opacity-50" />
-          <Vitest v-else-if="item.icon === 'vitest'" class="text-3xl opacity-50" />
+          <Slidev v-if="item.icon === 'slidev'" class="text-4xl opacity-50" />
+          <VueUse v-else-if="item.icon === 'vueuse'" class="text-4xl opacity-50" />
+          <VueReactivity v-else-if="item.icon === 'vue-reactivity'" class="text-4xl opacity-50" />
+          <VueDemi v-else-if="item.icon === 'vue-demi'" class="text-4xl opacity-50" />
+          <Unocss v-else-if="item.icon === 'unocss'" class="text-4xl opacity-50" />
+          <Vitest v-else-if="item.icon === 'vitest'" class="text-4xl opacity-50" />
           <div v-else class="text-3xl opacity-50" :class="item.icon || 'i-carbon-unknown'" />
         </div>
         <div class="flex-auto">
@@ -32,15 +32,15 @@ defineProps<{ projects: Record<string, any[]> }>()
       </a>
     </div>
   </template>
-  <div class="markdown mt-10">
-    <p class="opacity-75">
+  <div class="markdown pb5">
+    <p op75>
       <em>
         Thanks for getting intersted in my works! If like them or find them useful, consider
-        <a
+        &nbsp;<a
           href="https://github.com/sponsors/antfu"
           target="_blank"
           rel="nofollow noopener noreferrer"
-        >&nbsp;sponsoring me&nbsp;</a>to support me keeping them sustainable. Cheers! :)
+        >sponsoring me</a>&nbsp;to support me keeping them sustainable. Cheers! :)
       </em>
     </p>
   </div>
