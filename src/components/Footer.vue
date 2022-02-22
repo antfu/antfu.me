359367c79d74f74dcbf49498e31cfefa92ae8da0
Elliot Lintz <45725915+Elliot67@users.noreply.github.com>
2022-02-22 07:20:25 +0100

    chore: update footer year (#24)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-10 mb-6 prose m-auto opacity-50 flex">
-    <span class="text-sm"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2021 © Anthony Fu</span>
+    <span class="text-sm"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">CC BY-NC-SA 4.0</a> 2021-PRESENT © Anthony Fu</span>
     <div class="flex-auto" />
   </div>
 </template>
