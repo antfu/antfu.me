5a2c737b6bd1c71eb88389010066ae555e838a2e
Anthony Fu <anthonyfu117@hotmail.com>
2023-05-06 14:24:38 +0200

    feat: improve og

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
 useHead({
   meta: [
     { property: 'og:title', content: 'Anthony Fu' },
-    { property: 'og:image', content: 'https://antfu.me/og-icon.png' },
-    { name: 'description', content: 'Anthony Fu\'s Portfolio' },
-    { name: 'twitter:card', content: 'summary' },
+    { property: 'og:image', content: 'https://antfu.me/og.png' },
+    { property: 'og:description', content: 'Anthony Fu\'s Portfolio' },
+    { property: 'og:url', content: 'https://antfu.me' },
+    { property: 'og:type', content: 'website' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@antfu7' },
     { name: 'twitter:creator', content: '@antfu7' },
+    { name: 'twitter:title', content: 'Anthony Fu' },
+    { name: 'twitter:description', content: 'Anthony Fu\'s Portfolio' },
+    { name: 'twitter:image', content: 'https://antfu.me/og.png' },
   ],
 })
 </script>
