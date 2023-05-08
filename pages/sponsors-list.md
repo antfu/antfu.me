6476ae91cca20cce876b9583902023af2e2f90b3
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-05-08 23:16:03 +0800

    style: sponsor color transition (#48)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- pages/sponsors-list.md
+++ pages/sponsors-list.md
@@ -14,13 +14,7 @@ description: Sponsors of Anthony Fu
 </div>
 
 <div text-center p6>
-  <a
-    href="https://github.com/sponsors/antfu" target="_blank" 
-    mx-auto op50 px3 py2 rounded text-lg
-    hover="op100 text-rose bg-rose/10" class="group border-none!" >
-    <div i-carbon-favorite group-hover:i-carbon-favorite-filled />
-    Sponsor to support my work
-  </a>
+  <SponsorButton />
 </div>
 
 </div>
---
+++ src/components/SponsorButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    href="https://github.com/sponsors/antfu"
+    target="_blank"
+    mx-auto op50 px3 py2 rounded
+    transition-all duration-200 ease-out
+    hover="op100 text-rose bg-rose/10" class="group border-none!"
+  >
+    <div
+      i-carbon-favorite
+      group-hover="i-carbon-favorite-filled?mask text-rose"
+      transition-all duration-200 ease-out
+    />
+    Sponsor to support my work
+  </a>
+</template>
