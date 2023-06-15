bd9ec199ccc83c083dcf9c96795ffdce2fb646aa
Anthony Fu <anthonyfu117@hotmail.com>
2023-06-15 12:21:40 +0200

    feat: add afdian

--- src/components/SponsorButton.vue
+++ src/components/SponsorButton.vue
@@ -1,16 +1,32 @@
 <template>
-  <a
-    href="https://github.com/sponsors/antfu"
-    target="_blank"
-    mx-auto op50 px3 py2 rounded
-    transition-all duration-200 ease-out
-    hover="op100 text-rose bg-rose/10" class="group border-none!"
-  >
-    <div
-      i-carbon-favorite
-      group-hover="i-carbon-favorite-filled?mask text-rose"
+  <div flex="~ col">
+    <a
+      href="https://github.com/sponsors/antfu"
+      target="_blank"
+      mx-auto op50 px3 py2 rounded
       transition-all duration-200 ease-out
-    />
-    Sponsor to support my work
-  </a>
+      hover="op100 text-rose bg-rose/10" class="group border-none!"
+    >
+      <div
+        i-carbon-favorite
+        group-hover="i-carbon-favorite-filled?mask text-rose"
+        transition-all duration-200 ease-out
+      />
+      Sponsor to support my work
+    </a>
+    <a
+      href="https://afdian.net/a/antfu"
+      target="_blank"
+      mx-auto op50 px3 py2 rounded
+      transition-all duration-200 ease-out
+      hover="op100 text-yellow bg-yellow/10" class="group border-none!"
+    >
+      <div
+        i-carbon-flash
+        group-hover="i-carbon-flash-filled?mask text-yellow"
+        transition-all duration-200 ease-out
+      />
+      在爱发电上支持我
+    </a>
+  </div>
 </template>
