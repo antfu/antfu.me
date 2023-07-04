0bc8c30d1fd00a1afce45574b6b2a18e3eef2887
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-04 07:26:02 +0200

    chore: update deps

--- src/components/CalCom.vue
+++ src/components/CalCom.vue
@@ -12,37 +12,39 @@ declare global {
   }
 }
 
-window.Cal = window.Cal || function (...args: any[]) {
-  const p = function (a: any, ar: any) {
-    a.q.push(ar)
+if (typeof window !== 'undefined') {
+  window.Cal = window.Cal || function (...args: any[]) {
+    const p = function (a: any, ar: any) {
+      a.q.push(ar)
+    }
+
+    const cal = window.Cal
+    if (!cal.loaded) {
+      cal.ns = {}
+      cal.q = cal.q || []
+      document.head.appendChild(document.createElement('script')).src = 'https://app.cal.com/embed/embed.js'
+      cal.loaded = true
+    }
+    if (args[0] === 'init') {
+      const api = function () {
+        p(api, args)
+      } as any
+      const namespace = args[1]
+      api.q = api.q || []
+      typeof namespace === 'string'
+        ? (cal.ns[namespace] = api) && p(api, args)
+        : p(cal, args)
+      return
+    }
+    p(cal, args)
   }
 
-  const cal = window.Cal
-  if (!cal.loaded) {
-    cal.ns = {}
-    cal.q = cal.q || []
-    document.head.appendChild(document.createElement('script')).src = 'https://app.cal.com/embed/embed.js'
-    cal.loaded = true
-  }
-  if (args[0] === 'init') {
-    const api = function () {
-      p(api, args)
-    } as any
-    const namespace = args[1]
-    api.q = api.q || []
-    typeof namespace === 'string'
-      ? (cal.ns[namespace] = api) && p(api, args)
-      : p(cal, args)
-    return
-  }
-  p(cal, args)
+  window.Cal('init', { origin: 'https://app.cal.com' })
+
+  onMounted(() => {
+    window.Cal('ui', { theme: isDark.value ? 'dark' : 'light', styles: { branding: { brandColor: '#000000' } }, hideEventTypeDetails: false, layout: 'month_view' })
+  })
 }
-
-window.Cal('init', { origin: 'https://app.cal.com' })
-
-onMounted(() => {
-  window.Cal('ui', { theme: isDark.value ? 'dark' : 'light', styles: { branding: { brandColor: '#000000' } }, hideEventTypeDetails: false, layout: 'month_view' })
-})
 </script>
 
 <template>
