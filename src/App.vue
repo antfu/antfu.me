2b96ff594b5c9d2ea9dd28246abd6c2ccdeb87c8
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-09 00:47:56 +0200

    feat: image preview

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
 const route = useRoute()
+
+const imageModel = ref<HTMLImageElement>()
+
+useEventListener('click', (e) => {
+  const path = e.composedPath()
+  const first = path[0]
+  if (!(first instanceof HTMLElement))
+    return
+  if (first.tagName !== 'IMG')
+    return
+  if (!path.some(el => el instanceof HTMLDivElement && el.classList.contains('prose')))
+    return
+
+  imageModel.value = first as HTMLImageElement
+})
 </script>
 
 <template>
@@ -8,4 +23,10 @@ const route = useRoute()
     <RouterView />
     <Footer :key="route.path" />
   </main>
+  <Transition name="fade">
+    <div v-if="imageModel" fixed top-0 left-0 right-0 bottom-0 z-500 @click="imageModel = undefined">
+      <div absolute top-0 left-0 right-0 bottom-0 bg-black:80 z--1 />
+      <img :src="imageModel.src" :alt="imageModel.alt" w-full h-full object-contain>
+    </div>
+  </Transition>
 </template>
