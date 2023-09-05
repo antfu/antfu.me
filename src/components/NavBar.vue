ba3bde431f35b55fe4e33d2626fb2878c06d877b
DFMichelangelo <df.michelangelo@gmail.com>
2023-09-05 09:17:23 +0200

    changed logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,14 +13,15 @@ const { y: scroll } = useWindowScroll()
 
 <template>
   <header class="header z-40">
-    <RouterLink class="w-12 h-12 absolute xl:fixed m-5 select-none outline-none" to="/" focusable="false">
-      <Logo />
-    </RouterLink>
+
     <button title="Scroll to top" fixed right-3 bottom-3 w-10 h-10 hover:op100 rounded-full hover-bg-hex-8883 transition
       duration-300 z-100 print:hidden :class="scroll > 300 ? 'op30' : 'op0! pointer-events-none'" @click="toTop()">
       <div i-ri-arrow-up-line />
     </button>
     <nav class="nav">
+      <RouterLink class="w-12 h-12 absolute xl:fixed m-5 select-none outline-none" to="/" focusable="false">
+        <Logo />
+      </RouterLink>
       <div class="spacer" />
       <div class="right" print:op0>
         <RouterLink to="/posts" title="Blog">
