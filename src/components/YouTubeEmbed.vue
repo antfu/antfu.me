de28251fba096713f18b3622153182fd1eb7ebc3
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-27 16:16:27 +0100

    chore: update

--- src/components/YouTubeEmbed.vue
+++ src/components/YouTubeEmbed.vue
@@ -6,13 +6,15 @@ defineProps<{
 </script>
 
 <template>
-  <iframe
-    class="aspect-16/9 w-full rounded-lg shadow"
-    :class="noScale ? '' : 'lg:scale-120 lg:my-20'"
-    :src="`https://www.youtube-nocookie.com/embed/${id}`"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  />
+  <div>
+    <iframe
+      class="aspect-16/9 w-full rounded-lg shadow"
+      :class="noScale ? '' : 'lg:scale-120 lg:my-20'"
+      :src="`https://www.youtube-nocookie.com/embed/${id}`"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  </div>
 </template>
