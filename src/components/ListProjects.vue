e4f87f880bab9131521b0ada102d930417cfd7f7
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-09 02:50:48 +0100

    feat: update packages

--- src/components/ListProjects.vue
+++ src/components/ListProjects.vue
@@ -37,6 +37,7 @@ function slug(name: string) {
             <Unocss v-else-if="item.icon === 'unocss'" class="text-4xl opacity-50" />
             <Vitest v-else-if="item.icon === 'vitest'" class="text-4xl opacity-50" />
             <Elk v-else-if="item.icon === 'elk'" class="text-4xl opacity-50" />
+            <AnthonyFu v-else-if="item.icon === 'af'" class="text-4xl opacity-50" />
             <div v-else class="text-3xl opacity-50" :class="item.icon || 'i-carbon-unknown'" />
           </div>
           <div class="flex-auto">
